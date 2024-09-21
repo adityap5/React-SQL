008333b1fa064b70161b8bae9db54fa372802d59
adityap5 <aditya29pippal@gmail.com>
2024-09-22 00:19:04 +0530

    first

---
+++ Backend/index.js
@@ -0,0 +1,83 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+
+const app =express();
+app.use(cors());
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "@Apippal@666",
+    database: "test",
+});
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId);
+});
+
+app.get("/", (req, res) => {
+    res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    connection.query(q, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      return res.json(data);
+    });
+});
+
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.price,
+      req.body.cover,
+    ];
+  
+    connection.query(q, [values], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+});
+
+app.delete("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = " DELETE FROM books WHERE id = ? ";
+  
+    connection.query(q, [bookId], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+});
+
+app.put("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.price,
+      req.body.cover,
+    ];
+  
+    connection.query(q, [...values,bookId], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+});
+
+app.listen(8080,()=>{
+    console.log('listening on port 8080')
+});
